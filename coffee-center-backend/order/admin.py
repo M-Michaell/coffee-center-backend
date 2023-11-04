70596587f553eff63a8917d429deb37aee447b19
MagedKhaled <maged.khaled03@gmail.com>
2023-11-04 13:30:29 +0200

    add order module including order details, order items, pyment details, lacks connection with accounts and product models

--- coffee-center-backend/order/admin.py
+++ coffee-center-backend/order/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from order.models import OrderDetail, OrderItem, PaymentDetail
 # Register your models here.
+
+
+
+
+admin.site.register(OrderItem)
+admin.site.register(OrderDetail)
+admin.site.register(PaymentDetail)
