8201f0a9c66f0775a84dfed206350051f334f220
mohammed mostafa <mm994225@gmail.com>
2023-11-10 23:39:45 +0200

    get data from api

--- coffee-center-backend/product/api/views.py
+++ coffee-center-backend/product/api/views.py
@@ -12,7 +12,7 @@ def product_list(request):
     if request.method == 'GET':
         product = Product.objects.all()
         serializers = ProductSerializer(product, many=True)
-        return Response(serializers.data)
+        return Response({"list":serializers.data})
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializers = ProductSerializer(data=data)
